997a2f3f40fba8b496308c8603d48d5a5fc2137c
sohail chatoor <a-sochat@microsoft.com>
2017-10-23 17:13:54 +0200

    The meta writer seems to work... more tests needed

--- pysweep/spyview.py
+++ pysweep/spyview.py
@@ -1,11 +1,70 @@
 import os
 import numpy as np
+from collections import defaultdict
 
 from pysweep.output_formatter import BaseFormatter
 from pysweep.utils import DictMerge
 
 import qcodes
 import qcodes.data.location
+
+
+class SpyviewMetaWriter:
+    def __init__(self, output_file_path):
+
+        dirname, filename = os.path.split(output_file_path)
+        meta_file_name = filename.replace(".dat", ".meta.txt")
+        self._output_meta_file_path = os.path.join(dirname, meta_file_name)
+
+        self._independent_parameters = []
+        self._default_axis_properties = dict(max=0, min=np.inf, step=-1, length=1, name="")
+        self._axis_properties = defaultdict(lambda: self._default_axis_properties)
+
+    def add(self, spyview_buffer):
+
+        if not len(self._independent_parameters):
+            self._independent_parameters = [k for k in spyview_buffer if "independent_parameter" in spyview_buffer[k]]
+
+        property_names = ["length", "min", "max", "name"]
+        property_values = []
+        update = False
+        previous_axis_length = 1
+        for param in self._independent_parameters:
+
+            value = spyview_buffer[param]["value"]
+            mx = max(value)
+            mn = min(value)
+
+            if self._axis_properties[param]["max"] < mx:
+                self._axis_properties[param]["max"] = mx
+                update = True
+            else:
+                mx = self._axis_properties[param]["max"]
+
+            if self._axis_properties[param]["min"] > mn:
+                self._axis_properties[param]["min"] = mn
+                update = True
+            else:
+                mn = self._axis_properties[param]["min"]
+
+            self._axis_properties[param]["step"] = value[previous_axis_length] - value[0]
+            self._axis_properties[param]["name"] = param
+
+            self._axis_properties[param]["length"] = int((mx - mn) / self._axis_properties[param]["step"]) + 1
+            previous_axis_length = self._axis_properties[param]["length"]
+
+            s = "\n".join(str(self._axis_properties[param][k]) for k in property_names)
+            property_values.append(s)
+            property_names = ["length", "max", "min", "name"]
+
+        if not update:
+            return
+
+        if len(self._independent_parameters) < 3:
+            property_values.append("1\n0\n1\nnone")
+
+        with open(self._output_meta_file_path, "w") as fh:
+            fh.write("\n".join(property_values))
 
 
 class SpyviewFormatter(BaseFormatter):
@@ -32,6 +91,7 @@ class SpyviewFormatter(BaseFormatter):
         self._buffer = dict()
         self._merger = DictMerge(unit="replace", value="append", independent_parameter="replace")
         self._inner_sweep_start_value = None
+        self._meta_writer = SpyviewMetaWriter(self._output_file_path)
 
     def _get_buffer_size(self):
         if self._buffer == dict():
@@ -72,6 +132,7 @@ class SpyviewFormatter(BaseFormatter):
         with open(self._output_file_path, "a") as fh:
             fh.write("".join(lines))
 
+        self._meta_writer.add(self._buffer)
         self._buffer = dict()
 
     def add(self, dictionary):
@@ -84,3 +145,7 @@ class SpyviewFormatter(BaseFormatter):
     def finalize(self):
         if self._get_buffer_size() >= 2:
             self._write_buffer()
+
+    def output(self):
+        return self._output_file_path
+
