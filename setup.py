bc3c571bfa112d1ba36d345cd5fc26fa725c01b9
sohail chatoor <a-sochat@microsoft.com>
2017-11-09 18:03:26 +0100

    updated version in the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pysweep2',
-    version='0.1',
+    version='0.2',
     description='Easily sweep QCoDeS parameters',
     license='MIT',
     author='Sohail Chatoor',
