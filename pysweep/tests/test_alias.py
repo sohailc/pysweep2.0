7b486b873efaa9271daa57cbef6a0bc71575b842
sohail chatoor <a-sochat@microsoft.com>
2017-11-16 14:10:48 +0100

    Added basic aliasing ability

---
+++ pysweep/tests/test_alias.py
@@ -0,0 +1,30 @@
+import qcodes
+
+from pysweep.utils import alias
+
+
+class Powersource(qcodes.Instrument):
+    def __init__(self, name):
+        super().__init__(name)
+        self._voltage = 0
+
+        self.add_parameter(
+            name="voltage",
+            set_cmd=self._set_voltage,
+            get_cmd=self._get_voltage,
+            unit="mV"  # Stupid thing works in mV instead of Volt
+        )
+
+    def _set_voltage(self, value):
+        self._voltage = value
+
+    def _get_voltage(self):
+        return self._voltage
+
+
+def test():
+    pwr = Powersource("pwr")
+
+    gate = alias(pwr.voltage, new_label="gate voltage", new_name="gate", new_unit="V", conversion_factor=1E-3)
+    gate(3)  # Set at 3 V
+    print(gate())
