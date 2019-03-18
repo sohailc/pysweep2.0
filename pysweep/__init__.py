9f7eefb39985ba7a8c925c64710371cc006f240e
sochatoo <sohail.chatoor@microsoft.com>
2019-03-18 13:24:43 -0700

    Removed the old pysweep2 code in favour of the pysweep code in pytopo

--- pysweep/__init__.py
+++
@@ -1,7 +0,0 @@
-class Namespace:
-    pass
-
-from pysweep.sweep_object import sweep, nested_sweep, zip_sweep, time_trace, BaseSweepObject, hardware_sweep
-from pysweep.measurement import Measurement
-from pysweep.parameter_alias import Alias
-
--- pysweep/data_storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from pysweep.data_storage.spyview import SpyviewStorage
-from pysweep.data_storage.base_storage import BaseStorage, Delayed, DataSet
-from pysweep.data_storage.pandas_storage import PandasStorage
-
-
